a2cef55f7f53fda4c488d2b1601d103f858b0606
Lixing Wang <sabrina_xm@me.com>
2025-12-05 02:11:31 -0500

    gitignore

--- models/probe.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-class LinearProbe(tf.keras.Model):
-    def __init__(self, input_dim, num_classes):
-        super(LinearProbe, self).__init__()
-        self.dense = tf.keras.layers.Dense(num_classes, input_shape=(input_dim,))
-
-    def call(self, x):
-        return self.dense(x)
